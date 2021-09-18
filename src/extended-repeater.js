6df359d485af163c2cd1cb65024ed0e0e5752a53
prom1r <toforovdima@yandex.ru>
2021-09-18 20:14:52 +0300

    feat: develop simple chain, season, and repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,37 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+    const strRepeat = String(str);
+
+    if (!options) {
+        return strRepeat;
+    }
+
+    const additionRepeat = options.addition !== undefined ? String(options.addition) : '';
+    const separator = options.separator != null ? options.separator.toString() : '+';
+    const additionSeparator = options.additionSeparator != null ? options.additionSeparator : '|';
+    const repeatTimes = options.repeatTimes || 1;
+    const additionRepeatTimes = options.additionRepeatTimes || 1;
+
+    const additionRepeated = repeatString(additionRepeat, additionSeparator, additionRepeatTimes);
+    const strAndAddition = strRepeat + additionRepeated;
+
+    return repeatString(strAndAddition, separator, repeatTimes);
+}
+
+function repeatString(str, separator, repeatTimes) {
+    if (repeatTimes === 1) {
+        return str;
+    }
+
+    let strRepeat = '';
+    for (let i = 0; i < repeatTimes; i++) {
+        strRepeat += str;
+        if (i !== repeatTimes - 1) {
+            strRepeat += separator;
+        }
+    }
+
+    return strRepeat;
 }
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,40 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    arr: [],
+
+    getLength() {
+        return this.arr.length();
+    },
+
+    addLink(value) {
+        if (arguments.length === 0) {
+            this.arr.push('');
+        } else {
+            this.arr.push(value);
+        }
+
+        return this;
+    },
+    removeLink(position) {
+        if (this.arr[position - 1] === undefined) {
+            this.arr = [];
+            throw new Error('You can\'t remove incorrect link!')
+        }
+
+        this.arr.splice(position - 1, 1);
+
+        return this;
+    },
+    reverseChain() {
+        this.arr = this.arr.reverse();
+
+        return this;
+    },
+    finishChain() {
+        let linkStr = this.arr.map(x => `( ${x} )`);
+
+        this.arr = [];
+        return linkStr.join('~~');
+    }
 };
